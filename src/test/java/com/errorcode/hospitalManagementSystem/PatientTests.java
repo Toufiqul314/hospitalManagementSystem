41966949762068eb3cd4e097374c8125d83faf16
Toufiqul314 <islamtusher4@gmail.com>
2026-04-29 14:48:57 +0600

    add PatientTests class testTransactionMethod

--- src/test/java/com/errorcode/hospitalManagementSystem/PatientTests.java
+++ src/test/java/com/errorcode/hospitalManagementSystem/PatientTests.java
@@ -2,6 +2,7 @@ package com.errorcode.hospitalManagementSystem;
 
 import com.errorcode.hospitalManagementSystem.entity.Patient;
 import com.errorcode.hospitalManagementSystem.repository.PatientRepository;
+import com.errorcode.hospitalManagementSystem.service.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,9 @@ public class PatientTests {
     @Autowired
     private PatientRepository patientRepository;
 
+    @Autowired
+    private PatientService patientService;
+
     @Test
     public void testPatientRepository(){
         List<Patient>patientList = patientRepository.findAll();
@@ -21,5 +25,12 @@ public class PatientTests {
 
         Patient p1=new Patient();
         patientRepository.save(p1);
+    }
+
+    @Test
+    public void testTransactioMethods(){
+
+        Patient patient=patientService.getPatientById(1L);
+        System.out.println(patient);
     }
 }
