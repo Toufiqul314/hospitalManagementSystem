9b085711f3289e42fc42e0ec47104179ea693ea7
Toufiqul314 <islamtusher4@gmail.com>
2026-04-29 14:52:12 +0600

    add Patient class Table method

--- src/main/java/com/errorcode/hospitalManagementSystem/entity/Patient.java
+++ src/main/java/com/errorcode/hospitalManagementSystem/entity/Patient.java
@@ -1,12 +1,11 @@
 package com.errorcode.hospitalManagementSystem.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -15,19 +14,34 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 @ToString
+@Table(
+        name = "patient_tbl",
+        uniqueConstraints = {
+               // @UniqueConstraint(name = "unique_patient_email",columnNames = {"email"}),
+                @UniqueConstraint(name = "unique_patient_name_birthday",columnNames = {"name","birthDay"})
+        },
+        indexes = {
+                @Index(name = "idx_patient_birth_date",columnList = "birthDate")
+        }
+)
 public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "patient_name",nullable = false,length = 40)
     private String name;
 
     @ToString.Exclude
     private LocalDate birthDate;
 
+    @Column(unique = true,nullable = false)
     private  String email;
 
     private String gender;
 
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 }
