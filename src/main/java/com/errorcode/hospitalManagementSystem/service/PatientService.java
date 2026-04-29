c4caf06bef581d5d53b9c93c0ff6d70b879c449f
Toufiqul314 <islamtusher4@gmail.com>
2026-04-29 14:50:47 +0600

    add PatientService class Transactional method

--- src/main/java/com/errorcode/hospitalManagementSystem/service/PatientService.java
+++ src/main/java/com/errorcode/hospitalManagementSystem/service/PatientService.java
@@ -2,6 +2,8 @@ package com.errorcode.hospitalManagementSystem.service;
 
 import com.errorcode.hospitalManagementSystem.entity.Patient;
 import com.errorcode.hospitalManagementSystem.repository.PatientRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +13,18 @@ public class PatientService {
 
     private final PatientRepository patientRepository;
 
+   // private final EntityManager entityManager;
+
+    @Transactional
     public Patient getPatientById(Long id){
 
         Patient p1=patientRepository.findById(id).orElseThrow();
 
         Patient p2=patientRepository.findById(id).orElseThrow();
 
+        System.out.println(p1==p2);
+
+        p1.setName("toufiqul");
         return p1;
     }
 
