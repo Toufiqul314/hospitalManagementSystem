51cce5d165133445848fa9309283dd8c3eeb37a6
Toufiqul314 <islamtusher4@gmail.com>
2026-05-03 23:13:18 +0600

    add Doctor class

---
+++ src/main/java/com/errorcode/hospitalManagementSystem/entity/Doctor.java
@@ -0,0 +1,27 @@
+package com.errorcode.hospitalManagementSystem.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,length = 100)
+    private String name;
+
+    @Column(length = 100)
+    private String specialization;
+
+    @Column(nullable = false,unique = true,length = 100)
+    private String email;
+}
