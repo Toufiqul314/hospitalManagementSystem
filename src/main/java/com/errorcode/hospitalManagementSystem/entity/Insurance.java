339838bef447a2b78c0655b048cd917b92ca8b53
Toufiqul314 <islamtusher4@gmail.com>
2026-05-03 09:08:36 +0600

    add Insurance class

---
+++ src/main/java/com/errorcode/hospitalManagementSystem/entity/Insurance.java
@@ -0,0 +1,35 @@
+package com.errorcode.hospitalManagementSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class Insurance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true,nullable = false,length = 100)
+    private String policyNumber;
+
+    @Column(length = 100,nullable = false)
+    private String provider;
+
+    @Column(nullable = false)
+    private LocalDate validUntil;
+
+    @CreationTimestamp
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+}
